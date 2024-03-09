5655801f01c3fc53bf3f0b84180489065f4c33a4
Youssef Ali Mokhtar <youssef96mokhtar@gmail.com>
2024-03-09 12:43:06 +0200

    add section 5 express.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Potato');
+const path = require('path');
+
+const express = require('express');
+
+const mainRoutes = require('./routes/index');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(mainRoutes);
+
+app.listen(4000,()=>{
+    console.log('Server running on port 4000...');
+});
